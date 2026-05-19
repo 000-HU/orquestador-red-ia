1236be5cd7a431445aff1a89adfa1d8182c4df5e
Jared Rico Esquivel <tu-email@ejemplo.com>
2026-05-19 12:16:16 -0600

    Security: restrict server to localhost and fix CORS wildcard

--- master_orch.js
+++ master_orch.js
@@ -1,1 +1,17 @@
-const http = require('http'); const fs = require('fs'); http.createServer((req, res) => { res.setHeader('Access-Control-Allow-Origin', '*'); fs.readFile('datos_vector.bin', (err, data) => { if (err) { res.end('Error: No hay vectores'); return; } console.log('>>> Escaneando Red Escolar: ' + data.length + ' nodos detectados'); res.end(data); }); }).listen(3001, () => console.log('>>> MASTER ORCH (LAB PRIVADO) ACTIVO EN PUERTO 3001'));
+const http = require('http'); 
+const fs = require('fs'); 
+
+http.createServer((req, res) => { 
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost'); 
+    
+    fs.readFile('datos_vector.bin', (err, data) => { 
+        if (err) { 
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Error: No hay vectores'); 
+            return; 
+        } 
+        console.log('>>> Escaneando Red Escolar: ' + data.length + ' nodos detectados'); 
+        res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
+        res.end(data); 
+    }); 
+}).listen(3001, '127.0.0.1', () => console.log('>>> MASTER ORCH SEGURO - ACTIVO EN LOCALHOST:3001'));
